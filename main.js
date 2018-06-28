e45c2cc1105ec683dcd86b76937c734f18a05e20
Kevin_King <vegesaurus3@gmail.com>
2018-06-28 10:10:06 -0700

    render function plugged in, need to fix return data from api

--- main.js
+++ main.js
@@ -9,15 +9,32 @@ function getRefugeData(searchRes, callback) {
 	$.getJSON(refugeAPI_Search, query, callback);
 }
 
+function renderRes(result){
+	return`
+	<ul>
+		<li>${result.name}
+			<ul>
+				<li>${result.street}, ${result.city}</li>
+				<li>Accessible: ${result.accessible ? 'Yes' : 'No'}, Unisex: ${result.unisex ? 'Yes' : 'No'}, Changing Table: ${result.changing_table ? 'Yes' : 'No'}</li>
+
+			</ul>
+		</li>
+	</ul>
+`
+}
+
 function showRefugeData(data){
 	console.log(data);
+	const results= data.map((obj) => 
+	renderRes(obj));
+	$('.js-results').html(results)
 }
 	
 
 function submitTrigger() {
 	$('.js-form').submit(event => {
 		event.preventDefault();
-		const queryTarg= $(event.currentTarget).find('.js-search');
+		const queryTarg= $(event.currentTarget);
 		const query= queryTarg.val();
 		console.log(query);
 		queryTarg.val("");
